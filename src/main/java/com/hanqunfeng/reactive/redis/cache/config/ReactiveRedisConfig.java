d274bbdaa0d8d5b3f4214ae6ba8818f7dc0592af
hanqunfeng <hanqf2008@163.com>
2024-07-18 11:35:58 +0800

    自动配置类兼容springboot2.17以下的版本，支持最新的springboot3.3.1版本

--- src/main/java/com/hanqunfeng/reactive/redis/cache/config/ReactiveRedisConfig.java
+++ src/main/java/com/hanqunfeng/reactive/redis/cache/config/ReactiveRedisConfig.java
@@ -1,9 +1,14 @@
 package com.hanqunfeng.reactive.redis.cache.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -17,15 +22,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * @author hanqf
@@ -49,6 +59,74 @@ public class ReactiveRedisConfig {
      * 默认时间格式
      */
     public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+
+
+    private JavaTimeModule getJavaTimeModule() {
+        //LocalDateTime系列序列化和反序列化模块，继承自jsr310，我们在这里修改了日期格式
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        //序列化
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(
+                DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)));
+        javaTimeModule.addSerializer(LocalDate.class,
+                new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+        javaTimeModule.addSerializer(LocalTime.class,
+                new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
+        //反序列化
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(
+                DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)));
+        javaTimeModule.addDeserializer(LocalDate.class,
+                new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+        javaTimeModule.addDeserializer(LocalTime.class,
+                new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
+        return javaTimeModule;
+    }
+
+
+    private ObjectMapper jsonMapper() {
+        JsonMapper jsonMapper = JsonMapper.builder()
+                .visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+                // 去掉各种@JsonSerialize注解的解析
+                .configure(MapperFeature.USE_ANNOTATIONS, false)
+                // 遇到未知属性时不报错
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                // 空对象时不报错
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                // 只针对⾮空的值进⾏序列化
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .defaultDateFormat(new SimpleDateFormat(DEFAULT_DATE_TIME_FORMAT))
+                .defaultTimeZone(TimeZone.getTimeZone("GMT+8"))
+                //忽略重复模块注册
+                .disable(MapperFeature.IGNORE_DUPLICATE_MODULE_REGISTRATIONS)
+                //.activateDefaultTyping(LaissezFaireSubTypeValidator.instance , ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY)
+                .build();
+
+        // 会在json的结果中显示对象的类型信息
+        // 此项必须配置，否则会报java.lang.ClassCastException: java.util.LinkedHashMap cannot be cast to XXX
+        jsonMapper.activateDefaultTyping(jsonMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
+        //注册模块
+        jsonMapper.registerModule(getJavaTimeModule());
+
+        return jsonMapper;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = ReactiveRedisTemplate.class)
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory redisConnectionFactory) {
+        log.info("开启 RedisTemplate<String, Object>");
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(jsonMapper());
+
+        RedisSerializationContext<String, Object> context = RedisSerializationContext.<String, Object>newSerializationContext()
+                .key(stringSerializer)
+                .value(jackson2JsonRedisSerializer)
+                .hashKey(stringSerializer)
+                .hashValue(jackson2JsonRedisSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(redisConnectionFactory, context);
+    }
+
 
 
     @Bean
